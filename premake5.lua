3a8f38d2d4bd42a2e73f1ff18580cf18456707a4
Ntinos <konstantinides.kon@gmail.com>
2023-02-22 17:26:56 +0200

    Add files via upload

---
+++ premake5.lua
@@ -0,0 +1,47 @@
+project "ImGui"
+	kind "StaticLib" -- A static Library .lib
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	-- Include files
+	files
+	{
+		"imconfig.h",
+		"imgui.h",
+		"imgui.cpp",
+		"imgui_draw.cpp",
+		"imgui_internal.h",
+        "imgui_tables.cpp",
+		"imgui_widgets.cpp",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h",
+		"imgui_demo.cpp"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+
+	-- Debug Configs
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	-- Release Configs
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	-- Dist Config
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
